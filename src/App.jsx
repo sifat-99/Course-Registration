e58f9a53230c60037ee5504a666c45311cb61154
Md Abdur Rahman Sifat <mdabdurrahmansifat@gmail.com>
2023-09-15 03:24:28 +0600

    Cart addeded

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Courses from './Components/Courses/Courses'
+import Cart from './Components/Cart/Cart'
 
 function App() {
 
   return (
     <div>
       <Courses></Courses>
+      <Cart></Cart>
     </div>
   )
 }
